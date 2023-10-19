6ab9463bb92d923e1a7a83c843f32bfac87ae447
Ovishek Paul <ovishek.paul@gmail.com>
2023-10-19 10:31:10 +0600

    fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,9 @@ import store from "@redux/store";
 import { Provider } from "react-redux";
 
 function App() {
-  const test = "prettier";
-
-
-
-
-
   return (
     <Provider store={store}>
-      <div className='text-gray-200'>Let's rock 2, Starts here</div>
+      <div className='text-gray-200'>Lets rock, Starts here</div>
       <TestRedux />
     </Provider>
   );
