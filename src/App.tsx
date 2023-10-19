9adf55e18436a66a3d94beb13b832585dbc2824b
Ovishek Paul <ovishek.private@gmail.com>
2023-10-19 08:43:31 +0600

    fix redux absolute path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import "./App.css";
+import "@/App.css";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "@redux/store";
 
 function App() {
   return (
