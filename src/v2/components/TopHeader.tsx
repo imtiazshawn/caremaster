13497814ce39b09cd66963a15922444000e31238
Ovishek Paul <ovishek.paul@gmail.com>
2023-12-06 01:14:34 +0600

    Fix top header error issues (#42)
    * Fix top header error issues
    
    * cleanups

--- src/v2/components/TopHeader.tsx
+++ src/v2/components/TopHeader.tsx
@@ -1,20 +1,30 @@
 import dummyProfilePic from "@assets/dummy-profile-pic.png";
 import { ReactComponent as Notifications } from "@assets/notifications.svg";
-import { Tab, Tabs } from "@common/Tab";
-import { Column, FlexBox } from "@common/index";
+import { Box, Column, FlexBox } from "@common/index";
 import { ExpandMore } from "@mui/icons-material";
-import { TabContext } from "@mui/lab";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
+const topLinks = [
+  {
+    label: "Dashboard",
+    path: "/v2/dashboard",
+  },
+  {
+    label: "Calendar",
+    path: "/v2/calendar",
+  },
+  {
+    label: "Client",
+    path: "/v2/clients",
+  },
+  {
+    label: "Staff",
+    path: "/v2/staff",
+  },
+];
 
 export const TopHeader = () => {
-  const navigate = useNavigate();
   const location = useLocation();
-  // const [tabValue, setTabValue] = useState(currentTab);
-  // useEffect(() => {
-  //   if (location.pathname === "/v2/client") {
-  //     setTabValue("3");
-  //   }
-  // }, [location]);
 
   return (
     <Column sx={{ gap: 1, marginTop: 2 }}>
@@ -28,47 +38,31 @@ export const TopHeader = () => {
         }}
       >
         <FlexBox sx={{ flex: 1, justifyContent: "center" }}>
-          <TabContext value={location.pathname}>
-            <Tabs
-              value={location.pathname}
-              // onChange={(_, value) => {
-              //   setTabValue(value as string);
-              // }}
-            >
-              <Tab
-                label='Dashboard'
-                value='/v2/dashboard'
-                disableRipple
-                onClick={() => {
-                  navigate("/v2/dashboard");
-                }}
-              />
-              <Tab
-                label='Calendar'
-                value='/v2/calendar'
-                disableRipple
-                onClick={() => {
-                  navigate("/v2/calendar");
-                }}
-              />
-              <Tab
-                label='Client'
-                value='/v2/clients'
-                disableRipple
-                onClick={() => {
-                  navigate("/v2/clients");
-                }}
-              />
-              <Tab
-                label='Staff'
-                value='/v2/staff'
-                disableRipple
-                onClick={() => {
-                  navigate("/v2/staff");
-                }}
-              />
-            </Tabs>
-          </TabContext>
+          {topLinks.map((link) => {
+            const isActive = location.pathname === link.path;
+            return (
+              <Link
+                key={link.path}
+                to={link.path}
+              >
+                <Box
+                  sx={{
+                    cursor: "pointer",
+                    color: "#595959",
+                    p: 2,
+                    pb: 1,
+                    fontWeight: 600,
+                    ...(isActive && {
+                      borderBottom: "2px solid #595959",
+                      color: "#292929",
+                    }),
+                  }}
+                >
+                  {link.label}
+                </Box>
+              </Link>
+            );
+          })}
         </FlexBox>
         <FlexBox sx={{ gap: 1 }}>
           <Notifications />
